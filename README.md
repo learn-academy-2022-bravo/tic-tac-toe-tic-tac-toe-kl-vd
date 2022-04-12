8f4823e5bafdb364c67f9089929cf7f28a29c5d2
kmmontes <kmontes4989@gmail.com>
2022-04-12 15:18:01 -0700

    As a user, I shouldn't be able to click on a square that has already been selected.

--- README.md
+++ README.md
@@ -6,9 +6,15 @@ This project will challenge your knowledge of React but should also be an exerci
 
 ### User Stories:
 - As a user, I should start off seeing a 3x3 grid on the main page.
+<!-- we have a working 3x3 grid! -->
 - As a user, I should be able to click on a square to mark it.
+<!-- We have a working checkmark on the square -->
 - As a user, my partner should be able to click on a square after me and see their mark.
+<!--  -->
+<!-- We will have an X and an O and they will alternate
+ -->
 - As a user, I shouldn't be able to click on a square that has already been selected.
+<!-- We have alternating X & O in the squares -->
 - As a user, when someone has won the game (3 squares in a row: horizontally, vertically, or diagonally) I should see a notice telling me who won.
 - As a user, I should not be able to continue playing the game after the game has been won.
 - As a user, if there are no more squares available, I should see a notice telling me that the game has ended.
--- src/App.js
+++ src/App.js
@@ -6,15 +6,42 @@ class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+      squares: [null, null, null, null, null, null, null, null, null],
+      currentPlayer: "❌",
+      
+    }
+ 
+  }
+
+
+  handleGamePlay = (index) => {
+    const {squares, currentPlayer} = this.state
+    if(squares[index] === null) {
+      squares[index] = currentPlayer
+      this.setState({squares: squares, currentPlayer: currentPlayer === "❌"? "０": "❌"})
+    // } else if(index === currentPlayer2) {
+    //   squares[index] = "０"
+    //   this.setState({squares: squares})
+
     }
   }
 
-  render(){
+  render() {
     return(
       <>
         <h1>Tic Tac Toe</h1>
-        <Square />
+        <div className="gameboard">       
+          {this.state.squares.map((value, index) => {
+            return(
+               <Square 
+            key={index}
+            value={value}
+            index={index}
+            handleGamePlay={this.handleGamePlay}
+            />
+            )
+          })}
+        </div>
       </>
     )
   }
