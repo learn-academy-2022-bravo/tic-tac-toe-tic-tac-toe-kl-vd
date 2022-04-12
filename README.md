10175d7376ac7bf338fa76f9669788b8523c178d
Van Dexter <vanguardstellar@gmail.com>
2022-04-12 16:44:19 -0700

    still trying with win-conditions

--- README.md
+++ README.md
@@ -16,6 +16,8 @@ This project will challenge your knowledge of React but should also be an exerci
 - As a user, I shouldn't be able to click on a square that has already been selected.
 <!-- We have alternating X & O in the squares -->
 - As a user, when someone has won the game (3 squares in a row: horizontally, vertically, or diagonally) I should see a notice telling me who won.
+<!--  -->
+
 - As a user, I should not be able to continue playing the game after the game has been won.
 - As a user, if there are no more squares available, I should see a notice telling me that the game has ended.
 - As a user, I should see a restart button that when clicked will clear the gameboard.
--- src/App.js
+++ src/App.js
@@ -7,18 +7,51 @@ class App extends Component{
     super(props)
     this.state = {
       squares: [null, null, null, null, null, null, null, null, null],
-      currentPlayer: "❌",
-      
+      currentPlayer: "❌"
     }
- 
   }
+
+
+winner = () => {
+  const winningCombos = [
+    [1,2,3], // top row
+    [4,5,6], // middle row
+    [7,8,9], // bottom row
+    [1,4,7], // left column
+    [2,5,8], // middle column
+    [3,6,9], // right column
+    [1,5,9], // descending diagonal
+    [3,5,7] // ascending diagonal
+  ]
+  }
+
+  // const hasWinner = (grid: Grid): boolean => {
+  //   // map the grid values to the combo keys
+  //   const comboValues = winningCombos.map(
+  //     (comboKeys) => comboKeys.map(
+  //       (key) => grid[key]
+  //     )
+  //   )
+  //
+  //   // find returns a value or undefined
+  //   const maybeWinner = comboValues
+  //     .find(
+  //       (comboValues) =>
+  //         comboValues.every((v) => v === "❌") ||
+  //         comboValues.every((v) => v === "０")
+  //     );
+  //
+  //    // convert this value to a boolean
+  //    return !!maybeWinner
+  //  }
+
 
 
   handleGamePlay = (index) => {
     const {squares, currentPlayer} = this.state
     if(squares[index] === null) {
       squares[index] = currentPlayer
-      this.setState({squares: squares, currentPlayer: currentPlayer === "❌"? "０": "❌"})
+      this.setState({squares: squares, currentPlayer: currentPlayer === "❌" ? "０": "❌"})
     // } else if(index === currentPlayer2) {
     //   squares[index] = "０"
     //   this.setState({squares: squares})
@@ -30,10 +63,10 @@ class App extends Component{
     return(
       <>
         <h1>Tic Tac Toe</h1>
-        <div className="gameboard">       
+        <div className="gameboard">
           {this.state.squares.map((value, index) => {
             return(
-               <Square 
+               <Square
             key={index}
             value={value}
             index={index}
